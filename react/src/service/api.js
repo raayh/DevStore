5ec874a0d9cf1bf5844cee23af9e616b54d6f635
raayh <rayssasac04@gmail.com>
2021-09-28 11:57:39 -0300

    Limk alterado

--- react/src/service/api.js
+++ react/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'http://devschooloficial.herokuapp.com'
+    baseURL: 'https://site-devstore.herokuapp.com'
 })
 
 export default class Api {
